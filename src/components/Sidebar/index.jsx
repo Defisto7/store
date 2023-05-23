05ea794fd1e1437d669cd458010c3380ae94a9f2
Dima Yanovskji <criska7@gmail.com>
2023-05-23 10:23:27 +0300

    Master
    add sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import s from "../../styles/Sidebar.module.css";
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+    return (
+        <section className={s.sidebar}>
+            <div className={s.title}>CATEGORIES</div>
+            <nav>
+                <ul className={s.menu}>
+                    <li>
+                        <NavLink to={`/categories/${1}`}>Link</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className={s.footer}>
+                <a href="/help" target="_blank" className={s.link}>
+                    Help
+                </a>
+                <a
+                    href="/terms"
+                    target="_blank"
+                    className={s.link}
+                    style={{ textDecoration: "underline" }}
+                >
+                    Terms & Conditions
+                </a>
+            </div>
+        </section>
+    );
 };
 
 export default Sidebar;
