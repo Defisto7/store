62ebbc85ccb6d52c1f22af069c7436704166560b
Dima Yanovskji <criska7@gmail.com>
2023-05-23 12:06:00 +0300

    Master
    conected redux

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import AppRoutes from "../Routes";
 import Header from "../Header";
 import Footer from "../Footer";
 import Sidebar from "../Sidebar";
+import { getCategories } from "../../features/categories/categoriesSlice";
 
 const App = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch])
+
     return (
         <div className="app">
             <Header />
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import s from "../../styles/Sidebar.module.css";
-import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+    const { list } = useSelector(({ categories }) => categories);
+
     return (
         <section className={s.sidebar}>
             <div className={s.title}>CATEGORIES</div>
             <nav>
                 <ul className={s.menu}>
-                    <li>
-                        <NavLink to={`/categories/${1}`}>Link</NavLink>
+                    {list.map(({id, name}) => (
+                        <li key={id}>
+                        <NavLink className={({ isActive }) => `${s.link} ${isActive ? s.active : ""}`} to={`/categories/${id}`}>{name}</NavLink>
                     </li>
+                    ))}
                 </ul>
             </nav>
             <div className={s.footer}>
